26288ecf73751622ac3c35a2cbe858e92ce2efea
Hardik Gupta <hardikgupta0506@gmail.com>
2021-12-12 16:02:38 +0530

    slow

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -16,7 +16,7 @@ function Copyright(props) {
   return (
     <Typography variant="body2" color="text.secondary" align="center" {...props}>
       {'Copyright © '}
-      <Link color="inherit" href="https://mui.com/">
+      <Link color="inherit" href="/">
         <span className="icon">Bitastik</span>
       </Link>{' '}
       {new Date().getFullYear()}
@@ -104,12 +104,12 @@ export default function SignInSide() {
               </Button>
               <Grid container>
                 <Grid item xs>
-                  <Link href="#" variant="body2">
+                  <Link href="/" variant="body2">
                     Forgot password?
                   </Link>
                 </Grid>
                 <Grid item>
-                  <Link href="#" variant="body2">
+                  <Link href="/" variant="body2">
                     {"Don't have an account? Sign Up"}
                   </Link>
                 </Grid>
