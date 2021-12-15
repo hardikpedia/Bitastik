82d82b24b58a190587dcd028a8c8693dfee8bf5a
Hardik Gupta <hardikgupta0506@gmail.com>
2021-12-15 22:31:25 +0530

    commit

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import Login from './components/login/signIn'
+import React from "react";
+import ReactDOM from "react-dom";
+import "./styles/index.css";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
+
