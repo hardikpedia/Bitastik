05673a59fc51c33a59297f35f588ce68c4f61040
Hardik <hardikgupta0506@gmail.com>
2021-12-28 23:21:30 +0530

    calendar added\

--- pages/calender/index.js
+++ pages/calender/index.js
@@ -1,53 +1,72 @@
-import "react-big-calendar/lib/css/react-big-calendar.css"
-import { Calendar, momentLocalizer, Views } from 'react-big-calendar'
-import moment from 'moment'
-import React from "react"
-const ColoredDateCellWrapper = ({ children }) =>
-    React.cloneElement(React.Children.only(children), {
-        style: {
-            backgroundColor: 'grey',
-            opacity:0.5
-        },
-    })
+// import "react-big-calendar/lib/css/react-big-calendar.css"
+// import { Calendar, momentLocalizer, Views } from 'react-big-calendar'
+// import moment from 'moment'
+// import React from "react"
+// const ColoredDateCellWrapper = ({ children }) =>
+//     React.cloneElement(React.Children.only(children), {
+//         style: {
+//             backgroundColor: 'grey',
+//             opacity:0.5
+//         },
+//     })
 
-let allViews = Object.keys(Views).map(k => Views[k])
+// let allViews = Object.keys(Views).map(k => Views[k])
 
-const localizer = momentLocalizer(moment)
+// const localizer = momentLocalizer(moment)
 
 
-const now = new Date()
-console.log(now);
+// const now = new Date()
+// console.log(now);
 
-const myEventsList = [{
-    id: 0,
-    title: 'All Day Event very long title',
-    allDay: true,
-    start: new Date(2021, 29, 12),
-    end: new Date(2021, 29, 12),
-},
-{
-    id: 1,
-    title: 'Long Event',
-    start: new Date(2021, 11, 30),
-    end: new Date(2021, 11, 30),
-},
-]
-const MyCalendar = props => (
-    <div>
-        <Calendar
-            showMultiDayTimes
-            localizer={localizer}
-            events={myEventsList}
-            startAccessor="start"
-            endAccessor="end"
-            step={60}
-            style={{ height:"90vh" }}
-            views={allViews}
-            components={{
-                timeSlotWrapper: ColoredDateCellWrapper,
-            }}
-            selected
-        />
-    </div>
-)
-export default MyCalendar
+// const myEventsList = [{
+//     id: 0,
+//     title: 'All Day Event very long title',
+//     allDay: true,
+//     start: new Date(2021, 29, 12),
+//     end: new Date(2021, 29, 12),
+// },
+// {
+//     id: 1,
+//     title: 'Long Event',
+//     start: new Date(2021, 11, 30),
+//     end: new Date(2021, 11, 30),
+// },
+// ]
+// const MyCalendar = props => (
+//     <div>
+//         <Calendar
+//             showMultiDayTimes
+//             localizer={localizer}
+//             events={myEventsList}
+//             startAccessor="start"
+//             endAccessor="end"
+//             step={60}
+//             style={{ height:"90vh" }}
+//             views={allViews}
+//             components={{
+//                 timeSlotWrapper: ColoredDateCellWrapper,
+//             }}
+//             selected
+//         />
+//     </div>
+// )
+// export default MyCalendar
+
+import { StrictMode } from "react";
+import ReactBigCalendar from "../../components/calender/ReactBigCalendar"
+import React from 'react'
+
+function Calendar() {
+    return (
+        <div>
+            <StrictMode>
+                <ReactBigCalendar />
+            </StrictMode>
+        </div>
+    )
+}
+
+export default Calendar
+
+
+
