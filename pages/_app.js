dbea9478f973edf13653ab3cefc10bb90117abef
Hardik Gupta <hardikgupta0506@gmail.com>
2022-01-07 10:05:02 +0530

    commit

--- pages/_app.js
+++ pages/_app.js
@@ -4,59 +4,29 @@ import '../components/notes/styles.css'
 import Layout from '../components/layout/Layout'
 import { useAuthState } from 'react-firebase-hooks/auth'
 import { auth, signInWithGoogle } from '../firebase/firebase'
-import TypeIt from "typeit-react";
 import { useRouter } from 'next/router'
-
-const Landing = () => {
-  return (
-    <div>
-      <div>
-        <h1>
-          <TypeIt
-            options={{
-              strings: ["Bitastik"],
-              waitUntilVisible: true,
-            }}
-          />
-
-        </h1>
-      </div>
-      <div>
-        <h1>
-          <TypeIt
-            getBeforeInit={(instance) => {
-              instance.pause(1000).type("Making Bit Fantastic").pause(750).delete(6).pause(500).type("FuckingTastic !!!");
-              return instance;
-            }}
-            options={{
-              loop: true,
-            }}
-          />
-        </h1>
-      </div>
-
-    </div>
-  )
-}
-
+import Home from '.'
+import Land from '../components/layout/Land'
 function MyApp({ Component, pageProps }) {
   const [user] = useAuthState(auth)
   console.log(user)
   const router = useRouter()
   useEffect(() => {
-    // if (user) router.push('/dashboard')
+    if (user) router.push('/dashboard')
   }, [user])
   return (
     <> {user ?
       <Layout>
         <Component {...pageProps} />
-      </Layout> : <div>
-        <Landing />
-        <button onClick={async () => {
+      </Layout> :
+      <Land>
+        <div>
+          <Home />
+        </div>
+        <button className='button' onClick={async () => {
           await signInWithGoogle();
-        }}>Login</button>
-
-      </div>
+        }}><span>Login</span></button>
+      </Land>
 
     }
     </>
