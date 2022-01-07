2fe2f723b5847f338bf0821ce2c590ba3345b244
Hardik Gupta <hardikgupta0506@gmail.com>
2022-01-07 12:32:44 +0530

    commit

--- pages/todolist/index.js
+++ pages/todolist/index.js
@@ -4,24 +4,41 @@ import dbConnect from "../../lib/dbconnect";
 import Todo from '../../models/Todo'
 import { useAuthState } from "react-firebase-hooks/auth";
 import { auth } from "../../firebase/firebase";
-
+import { useEffect } from "react";
 const Documents = ({todo}) => {
   const [user] = useAuthState(auth);
+  useEffect(() => {
+    const fetchData = async () => {
+      const todo = await fetch('/api/todo', {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ uid: user.uid })
+      })
+      console.log(todo)
+    }
+    fetchData();
+  }, [])
+
+
+ 
   return <Todolist todo={todo}></Todolist>;
+
 };
 
 export default Documents
 
-export async function getStaticProps() {
+// export async function getStaticProps() {
   
-    await dbConnect();
-    const data = await Todo.find({});
-    const todo = JSON.parse(JSON.stringify(data))
+//     await dbConnect();
+//     const data = await Todo.find({});
+//     const todo = JSON.parse(JSON.stringify(data))
     
-    return {
-      props: {
-        todo,
-      },
-      revalidate: 60,
-    };
-  }
+//     return {
+//       props: {
+//         todo,
+//       },
+//       revalidate: 60,
+//     };
+//   }
