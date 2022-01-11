db0fd45546771b9c6f330088bff8a99b205fa6e2
Hardik Gupta <hardikgupta0506@gmail.com>
2022-01-11 11:56:15 +0530

    commit

--- components/notes/Header.jsx
+++ components/notes/Header.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import HighlightIcon from "@material-ui/icons/Highlight";
-
+import Image from "next/image";
+import noting from '../../assets/noty.png'
 function Header() {
   return (
     <header>
       <h1>
         {" "}
-        <HighlightIcon />
+        <Image src={noting} height={"40px"} width={"40px"}/>
         BITNotes
       </h1>
     </header>
